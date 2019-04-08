890a5e30f835235e8fb58ecfc9c857dafeb16e9e
limeee7 <110997fdnj>
2019-04-08 19:32:09 +0200

    .

---
+++ app/src/main/java/com/example/jowisz/ProfileFragment.java
@@ -0,0 +1,143 @@
+package com.example.jowisz;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ProfileFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    private EditText name;
+    private EditText surname;
+    private EditText oldPassword;
+    private EditText newPassword;
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        name = getActivity().findViewById(R.id.profileNameTextField);
+        surname = getActivity().findViewById(R.id.profileSurnameTextField);
+        oldPassword = getActivity().findViewById(R.id.profileOldPasswordTextField);
+        newPassword = getActivity().findViewById(R.id.profileNewPasswordTextField);
+
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public void saveChanges(View v){
+        StringBuilder errors = new StringBuilder();
+        String password = this.oldPassword.getText().toString();
+        String repeatedPassword = this.newPassword.getText().toString();
+        String name = this.name.getText().toString();
+        String surname = this.surname.getText().toString();
+
+        if (!password.equals(repeatedPassword)) {
+            errors.append("Hasłą się nie zgadzają");
+            this.oldPassword.setBackgroundColor(getResources().getColor(R.color.errorLight));
+            this.newPassword.setBackgroundColor(getResources().getColor(R.color.errorLight));
+        }
+        if (name.matches(".*\\d.*")){
+            errors.append("Imię musi zawierać tylko litery");
+            this.name.setBackgroundColor(getResources().getColor(R.color.errorLight));
+        }
+        if (surname.matches(".*\\d.*")){
+            errors.append("Nazwisko musi zawierać tylko litery");
+            this.surname.setBackgroundColor(getResources().getColor(R.color.errorLight));
+        }
+    }
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
